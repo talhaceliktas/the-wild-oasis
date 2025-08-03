46bc591dda6099b3864981e8062160758021d6ff
sleepwhy <avrol928@gmail.com>
2025-08-03 23:20:25 +0300

    sales grafigi eklendi

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,10 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+import Stats from "./Stats";
+import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +12,32 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+const DashboardLayout = () => {
+  const { bookings, isPending } = useRecentBookings();
+  const {
+    stays,
+    isPending: isPending2,
+    confirmedStays,
+    numDays,
+  } = useRecentStays();
+  const { data: cabins, isLoading: isPending3 } = useCabins();
+
+  if (isPending || isPending2 || isPending3) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins?.length}
+      />
+      <div>Today's activity</div>
+      <div>Chart stay durations</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
+    </StyledDashboardLayout>
+  );
+};
+
+export default DashboardLayout;
